f9eec8a0e025c552ba56313add06855ccbe0a40d
Tyler Wright <tyler.wright@fullstackacademy.com>
2023-07-17 12:23:47 -0700

    added form for creating recipes

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const API_URL = "https://fsa-async-await.herokuapp.com/api/demo/recipes/";
 
-const recipiesContainer = document.querySelector("#recipes-container");
+const recipesContainer = document.querySelector("#recipes-container");
+const formContainer = document.querySelector("#form-container");
 
 const fetchURL = async (url) => {
   try {
@@ -59,27 +60,86 @@ const removeRecipe = async (id) => {
   }
 };
 
-const init = async () => {
+// Append each recipe to the page
+const renderAllRecipes = (recipeList) => {
+  //check for empty/undefined lists
+  if (!recipeList || recipeList.length === 0) {
+    recipesContainer.innerHTML = "<h3>No recipes found</h3>";
+    return;
+  }
+
+  //Clear the page
+  recipesContainer.innerHTML = "";
+
+  //Define each recipe
+  recipeList.forEach((recipe) => {
+    //create element
+    const recipeElement = document.createElement("div");
+    recipeElement.classList.add('recipe-card');
+    //add details
+    recipeElement.innerHTML = `
+      <h4>${recipe.title}</h4>
+      <img class="recipe-img" src="${recipe.image_url}" alt="${recipe.title}"/>
+      <p>${recipe.instructions}</p>
+    `;
+
+    //TODO
+    //create a button with a listner
+    //handler should call removeRecipe
+
+    //append to container
+    recipesContainer.appendChild(recipeElement);
+  });
+};
+
+//Create a form that will create a new recipe when submitted
+const createNewRecipeForm = () => {
+  // create the form (Created in HTML)
+
+  // formContainer.innerHTML = `
+  // <form>
+  // <label for="title">Title</label>
+  // <input type="text" id="title" name="title" placeholder="Enter Title" />
+  // <label for="img_url">Image URL</label>
+  // <input type="text" id="image_url" name="image_url" placeholder="ImageURL" />
+  // <label for="instructions">Instructions</label>
+  // <textarea id="instructions" name="instructions" placeholder="Enter Instructions"></textarea>
+  // <button type="submit">Create</button>
+  // </form>
+  // `;
+
+  const form = formContainer.querySelector('form');
+
+  // Set up event listener for the form
+  form.addEventListener('submit', submitHandler);
+}
+
+const submitHandler = async (event) => {
+  event.preventDefault();
+  //eventlistener should call createNewRecipe 
+  const form = event.target;
+
+  const title = form.title.value;
+  const image_url = form.image_url.value;
+  const instructions = form.instructions.value;
+
+  await createNewRecipe(title, image_url, instructions);
+
+  form.title.value = "";
+  form.image_url.value = "";
+  form.instructions.value = "";
+ 
+  
   const recipes = await fetchAllRecipes();
+  renderAllRecipes(recipes);
+}
 
-  console.log("RECIPES", recipes);
+const init = async () => {
 
-  // const hamburger = await fetchSingleRecipe(580);
-  // //console.log("HAMBURGER", hamburger);
+  createNewRecipeForm();
 
-  // const title = "Chimichanga55";
-  // const image_url = "https://thesaltedpepper.com/wp-content/uploads/2020/04/chimichanga-sq.jpg";
-  // const instructions = "Fry a burrito and eat up!";
-
-  // const newRecipe = await createNewRecipe(title, image_url, instructions);
-  // console.log("CHIMI?", newRecipe);
-
-  const deleted = await removeRecipe(1848);
-  console.log("DELETED: ", deleted);
-
-  const newRecipes = await fetchAllRecipes();
-
-  console.log("NEW Recipes", newRecipes);
+  const recipes = await fetchAllRecipes();
+  renderAllRecipes(recipes);
 };
 
 init();
